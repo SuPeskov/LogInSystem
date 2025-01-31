0f1a81ac44e9bcb38c6ff6b84bbb1c6ba448e8d8
SuPeskov <supeskov@yandex.ru>
2025-01-31 13:26:46 +0300

    создано меню выбора продуктов

--- login.js
+++ login.js
@@ -3,3 +3,10 @@ const token = sessionStorage.getItem('token');
 if ((!token) || (token == 'undefined')) {
     window.location.href = './start.html';
 }
+/* const exitSite = document.querySelector('#exitBtn');
+
+exitSite.addEventListener('click', (event) => {
+    event.preventDefault();
+    login();
+  
+  })
