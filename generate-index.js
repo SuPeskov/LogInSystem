391ca817c2fb0cbe7a0e33e872bfae4600aae8fb
SuPeskov <supeskov@yandex.ru>
2025-03-06 17:50:44 +0300

    добавлена функция поиска

---
+++ generate-index.js
@@ -0,0 +1,33 @@
+// generate-index.js
+const fs = require('fs');
+const path = require('path');
+const { JSDOM } = require('jsdom');
+
+const rootDir = './.'; // Путь к корню сайта
+const index = [];
+
+function processDirectory(dir) {
+  const files = fs.readdirSync(dir);
+  
+  files.forEach(file => {
+    const filePath = path.join(dir, file);
+    const stat = fs.statSync(filePath);
+    
+    if (stat.isDirectory()) {
+      processDirectory(filePath);
+    } else if (path.extname(file) === '.html') {
+      const content = fs.readFileSync(filePath, 'utf-8');
+      const dom = new JSDOM(content);
+      const text = dom.window.document.body.textContent.replace(/\s+/g, ' ');
+      const relativePath = path.relative(rootDir, filePath);
+      
+      index.push({
+        path: relativePath,
+        content: text
+      });
+    }
+  });
+}
+
+processDirectory(rootDir);
+fs.writeFileSync('search-index.json', JSON.stringify(index));
