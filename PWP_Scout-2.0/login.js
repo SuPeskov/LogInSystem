f38ea89d5868c0d894cc2d98ae79d97687d29b0c
SuPeskov <supeskov@yandex.ru>
2025-03-13 17:47:17 +0300

    обновлена схема с хэдером в папке со Скаутом

--- PWP_Scout-2.0/login.js
+++ PWP_Scout-2.0/login.js
@@ -3,3 +3,8 @@
 if ((!token) || (token == 'undefined')) {
     window.location.href = 'https://supeskov.github.io/LogInSystem/';
 } */
+    fetch('header.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('header-container').innerHTML = data;
+        });
